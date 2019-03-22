4f787eb2fe09949e58e4aba41f87d3572243d999
James U <james.ulyett@gmail.com>
2019-03-22 16:10:46 +0000

    create dice function return list

---
+++ dice.py
@@ -0,0 +1,20 @@
+from random import randint
+
+# rolls two dice and returns the sum and boolean of double throw
+
+def dice_roll():
+    roll = []
+    die_1 = randint(1, 6)
+    die_2 = randint(1, 6)
+    roll.append((die_1 + die_2))
+    if die_1 == die_2:
+        roll.append(True)
+    else: 
+        roll.append(False)
+    print(die_1)
+    print(die_2)
+    print(roll)
+
+    return roll
+
+dice_roll()
