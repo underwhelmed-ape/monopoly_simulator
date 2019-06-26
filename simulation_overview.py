910d1114ff7e0cfc1f1647ba9d0be1befc50a40c
underwhelmed-ape <james.ulyett@gmail.com>
2019-06-26 10:55:57 +0100

    jupyter notebook of process
    Signed-off-by: underwhelmed-ape <james.ulyett@gmail.com>

---
+++ simulation_overview.py
@@ -0,0 +1,26 @@
+#%% Import packages
+
+from random import randint    
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+from dice import dice_roll
+
+
+
+#%% Dice roll statistics
+
+
+rolls = {}
+for i in range(10000):
+    roll = dice_roll()[0]
+    if roll in rolls:
+        rolls[roll] += 1
+    else:
+        rolls[roll] = 1
+
+print(rolls)
+print(rolls.items())
+
+plt.bar(list(rolls.keys()), rolls.values(), color='g')
