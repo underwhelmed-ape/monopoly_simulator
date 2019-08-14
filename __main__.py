543deb7b819b8f4bd7eef0ddbad974b0b3ad87d3
underwhelmed-ape <james.ulyett@gmail.com>
2019-08-14 09:24:10 +0100

    results of games
    Signed-off-by: underwhelmed-ape <james.ulyett@gmail.com>

--- __main__.py
+++ __main__.py
@@ -6,7 +6,7 @@ from cards import select_chance_card, select_community_chest_card
 from player_movement import move_player
 
 games = 0 # count the number of games played
-number_of_games = 500000 # number of simulated games to play
+number_of_games = 1000000 # number of simulated games to play
 
 print(f'Begin {number_of_games} games')
 
@@ -160,3 +160,45 @@ print(xtab.mean(axis=0))
 # 28    2.840913
 # 33    2.418398
 # dtype: float64
+
+
+# Begin 1000000 games ~ 2 days
+# 0     2.983781
+# 1     2.369871
+# 2     2.075961
+# 3     2.451667
+# 5     2.893929
+# 6     2.569872
+# 7     1.769340
+# 8     2.600356
+# 9     2.568416
+# 10    5.766926
+# 11    2.520752
+# 12    2.569726
+# 13    2.984999
+# 14    2.673248
+# 15    2.793462
+# 16    2.905395
+# 17    3.017851
+# 18    2.979168
+# 19    3.376396
+# 20    2.963484
+# 21    2.931923
+# 22    1.907747
+# 23    2.886217
+# 24    3.300947
+# 25    2.902301
+# 26    2.898193
+# 27    2.876638
+# 28    2.836137
+# 29    2.800596
+# 31    2.830812
+# 32    2.764006
+# 33    2.423212
+# 34    2.649673
+# 35    2.585479
+# 36    1.707672
+# 38    2.385478
+# 39    2.745127
+# 4     2.601548
+# 37    2.387002
