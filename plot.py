8f94f413034f5384670a3c460611ad8b6bc372a2
James <james.ulyett@gmail.com>
2019-11-07 17:25:06 +0000

    plotting
    Signed-off-by: James <james.ulyett@gmail.com>

--- plot.py
+++ plot.py
@@ -1,2 +1,22 @@
-import matplotlib
-import matplotlib.pyplot 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0,5,11)
+y = x ** 2
+
+plt.plot(x,y, 'r-')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+plt.title('A main title')
+# plt.show()
+
+
+# making two subplots
+plt.subplot(1,2,1)
+plt.plot(x,y,'b-')
+
+plt.subplot(1,2,2)
+plt.plot(x,y,'r-')
+
+plt.show()
